12b8b9a9ee6ef70e1ba7fa8f90783d2675423123
MandaiRyuta <Mandai1990@outlook.jp>
2018-12-27 02:37:57 +0900

    マージしてバグを修正した状態

--- MainWindow/Views/MainWindow.xaml.cs
+++ MainWindow/Views/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,67 +46,5 @@ namespace ZuneLikeWindow.Views
         {
             this.Close();
         }
-
-        private void ToolListMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.toolListScroller.ScrollToHorizontalOffset(this.toolListScroller.HorizontalOffset);
-        }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ZuneLikeWindow.Views
-{
-	/// <summary>
-	/// MainWindow.xaml の相互作用ロジック
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			this.InitializeComponent();
-
-			this.MouseLeftButtonDown += (sender, e) => this.DragMove();
-		}
-
-        private void HideButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Maximized;
-        }
-
-        private void MinimizeButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Normal;
-        }
-
-        private void CloseButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-        }
-    }
-}
->>>>>>> TerrainInformation
